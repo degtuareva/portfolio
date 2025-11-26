5e71ce92f1a345da5b145b843fe53263b7a1c222
olga.olegovna.603 <olga.olegovna.603@list.ru>
2025-11-26 04:38:06 +0300

    доработка 2

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
 const projects = [
-  { name: 'User Service (Java + Docker + Testing)', url: 'https://github.com/degtuareva/user-service.git', categories: ['java', 'docker', 'testing'] },
-  { name: 'Message Service (Java + Docker)', url: 'https://github.com/degtuareva/message-service.git', categories: ['java', 'docker'] },
-  { name: 'Image Service (Java + Docker)', url: 'https://github.com/degtuareva/image-service.git', categories: ['java', 'docker'] },
-  { name: 'Common Library (Java)', url: 'https://github.com/degtuareva/common-lib.git', categories: ['java'] },
-  { name: 'Discovery Service (Java + Docker)', url: 'https://github.com/degtuareva/discovery-service.git', categories: ['java', 'docker'] },
-  { name: 'API Gateway Service (Java)', url: 'https://github.com/degtuareva/api-gateway-service.git', categories: ['java'] },
-  { name: 'Auth Service (Java)', url: 'https://github.com/degtuareva/auth-service.git', categories: ['java'] },
-  { name: 'JCalc Project (Java)', url: 'https://github.com/degtuareva/jcalc.iml.git', categories: ['java'] },
-  { name: 'GAME (JavaScript)', url: 'https://github.com/degtuareva/GAME.git', categories: ['javaScript'] },
-  { name: 'Pizza (JavaScript)', url: 'https://github.com/degtuareva/pizza.git', categories: ['javaScript'] }
+  {
+    name: 'User Service (Java + Docker + Testing)',
+    url: 'https://github.com/degtuareva/user-service.git',
+    categories: ['java', 'docker', 'testing']
+  },
+  {
+    name: 'Message Service (Java + Docker)',
+    url: 'https://github.com/degtuareva/message-service.git',
+    categories: ['java', 'docker']
+  },
+  {
+    name: 'Image Service (Java + Docker)',
+    url: 'https://github.com/degtuareva/image-service.git',
+    categories: ['java', 'docker']
+  },
+  {name: 'Common Library (Java)', url: 'https://github.com/degtuareva/common-lib.git', categories: ['java']},
+  {
+    name: 'Discovery Service (Java + Docker)',
+    url: 'https://github.com/degtuareva/discovery-service.git',
+    categories: ['java', 'docker']
+  },
+  {
+    name: 'API Gateway Service (Java)',
+    url: 'https://github.com/degtuareva/api-gateway-service.git',
+    categories: ['java']
+  },
+  {name: 'Auth Service (Java)', url: 'https://github.com/degtuareva/auth-service.git', categories: ['java']},
+  {name: 'JCalc Project (Java)', url: 'https://github.com/degtuareva/jcalc.iml.git', categories: ['java']},
+  {name: 'GAME (JavaScript)', url: 'https://github.com/degtuareva/GAME.git', categories: ['javaScript']},
+  {name: 'Pizza (JavaScript)', url: 'https://github.com/degtuareva/pizza.git', categories: ['javaScript']}
 ];
 
 const projectList = document.getElementById('projectList');
@@ -16,7 +36,7 @@ const filterButtons = document.querySelectorAll('.filter-btn');
 
 function displayProjects(filteredProjects) {
   projectList.innerHTML = '';
-  if(filteredProjects.length === 0) {
+  if (filteredProjects.length === 0) {
     projectList.innerHTML = '<li class="text-center text-gray-500 col-span-full" tabindex="0">Нет проектов для отображения</li>';
     return;
   }
@@ -37,7 +57,7 @@ function displayProjects(filteredProjects) {
 }
 
 function filterProjects(category) {
-  if(category === 'all') {
+  if (category === 'all') {
     return projects;
   }
   return projects.filter(project => project.categories.includes(category));
@@ -56,6 +76,32 @@ filterButtons.forEach(btn => {
     const filtered = filterProjects(filter);
     displayProjects(filtered);
   });
+});
+
+const dropdownBtn = document.getElementById("dropdownBtn");
+const dropdownMenu = document.getElementById("dropdownMenu");
+
+//открываем меню
+dropdownBtn.addEventListener('click', function (e) {
+  dropdownMenu.classList.toggle('open');
+});
+
+document.addEventListener("click", function (e) {
+  // НЕ закрываем, если клик по ссылке или по меню
+  if (!dropdownMenu.contains(e.target) && e.target !== dropdownBtn) {
+    dropdownMenu.classList.remove("open");
+  }
+});
+
+
+document.addEventListener("click", function (e) {
+  // Если клик был вне меню и не по кнопке, закрываем меню
+  if (
+    !dropdownMenu.contains(e.target) &&
+    e.target !== dropdownBtn
+  ) {
+    dropdownMenu.classList.remove("open");
+  }
 });
 
 // Показываем все проекты по умолчанию
